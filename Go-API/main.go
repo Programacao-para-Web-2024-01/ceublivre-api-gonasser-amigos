0c453f416dc2dac7375a8c0c241cf9c90725a14b
Átila Alves <53942371+AtilaAlvesRodrigues@users.noreply.github.com>
2024-04-29 09:21:25 -0300

    Update main.go
    update final

--- Go-API/main.go
+++ Go-API/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,11 +14,18 @@ type Item struct {
 	Quantity int    `json:"quantity"`
 }
 
+type WishlistItem struct {
+	UserID string `json:"user_id"`
+	ItemID string `json:"item_id"`
+}
+
 var inventory = []Item{
 	{ID: "1", Nome: "In Search of Lost Time", Valor: "30.00", Quantity: 2},
 	{ID: "2", Nome: "The Great Gatsby", Valor: "50.00", Quantity: 5},
 	{ID: "3", Nome: "War and Peace", Valor: "25.50", Quantity: 6},
 }
+
+var wishlist = make(map[string][]string)
 
 func getItem(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, inventory)
@@ -87,6 +95,7 @@ func getItemByIDFromInventory(id string) (*Item, error) {
 	return nil, errors.New("item not found")
 }
 
+//Criando item
 func createItem(c *gin.Context) {
 	var newItem Item
 
@@ -99,6 +108,55 @@ func createItem(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newItem)
 }
 
+//adicionando a wishlist
+func addToWishlist(c *gin.Context) {
+	userID := c.Query("user_id")
+	itemID := c.Query("item_id")
+
+	if userID == "" || itemID == "" {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "missing user_id or item_id query parameter"})
+		return
+	}
+
+	wishlistItems, ok := wishlist[userID]
+	if !ok {
+		wishlistItems = []string{}
+	}
+
+	wishlistItems = append(wishlistItems, itemID)
+	wishlist[userID] = wishlistItems
+
+	c.IndentedJSON(http.StatusCreated, gin.H{"message": "item added to wishlist"})
+}
+
+//remover da wishlist
+func removeFromWishlist(c *gin.Context) {
+	userID := c.Query("user_id")
+	itemID := c.Query("item_id")
+
+	if userID == "" || itemID == "" {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "missing user_id or item_id query parameter"})
+		return
+	}
+
+	wishlistItems, ok := wishlist[userID]
+	if !ok {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "wishlist not found for user"})
+		return
+	}
+
+	for i, id := range wishlistItems {
+		if id == itemID {
+			wishlistItems = append(wishlistItems[:i], wishlistItems[i+1:]...)
+			wishlist[userID] = wishlistItems
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "item removed from wishlist"})
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "item not found in wishlist"})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/item", getItem)
@@ -106,6 +164,8 @@ func main() {
 	router.POST("/item", createItem)
 	router.PATCH("/checkout", checkoutItem)
 	router.PATCH("/return", returnItem)
+	router.POST("/wishlist/add", addToWishlist)
+	router.DELETE("/wishlist/remove", removeFromWishlist)
 	router.Run("localhost:8080")
 }
 
